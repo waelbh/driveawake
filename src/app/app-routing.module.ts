92e6079a6a5397f8ce4a254a8188c29d5cabbf2b
waelbh <47089212+waelbh@users.noreply.github.com>
2020-02-08 00:45:43 +0100

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,50 @@
 import { NgModule } from '@angular/core';
+import { CommonModule, } from '@angular/common';
+import { BrowserModule  } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { AdminLayoutComponent } from './dashboard/layouts/admin-layout/admin-layout.component';
+import { AuthLayoutComponent } from './dashboard/layouts/auth-layout/auth-layout.component';
+
+const routes: Routes =[
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
+  }, {
+    path: '',
+    component: AdminLayoutComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: './dashboard/layouts/admin-layout/admin-layout.module#AdminLayoutModule'
+      }
+    ]
+  }, {
+    path: '',
+    component: AuthLayoutComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: './dashboard/layouts/auth-layout/auth-layout.module#AuthLayoutModule'
+      }
+    ]
+  }, {
+    path: '**',
+    redirectTo: 'dashboard'
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    CommonModule,
+    BrowserModule,
+    RouterModule.forRoot(routes,{
+      useHash: true
+    })
+  ],
+  exports: [
+  ],
 })
 export class AppRoutingModule { }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ComponentsModule } from './dashboard/components/components.module';
+import { AdminLayoutComponent } from './dashboard/layouts/admin-layout/admin-layout.component';
+import { AuthLayoutComponent } from './dashboard/layouts/auth-layout/auth-layout.component';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TestComponent } from './sharedComponent/test/test.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminLayoutComponent,
+    AuthLayoutComponent,
+    TestComponent
+    
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    ComponentsModule,
+    NgbModule,
+    RouterModule,
     AppRoutingModule
   ],
   providers: [],
