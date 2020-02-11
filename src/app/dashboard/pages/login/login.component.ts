2533d9fb9a74dbb0c36b6a57db42b74bf1b0bc18
onlyifihadtime <kanzadounichway@gmail.com>
2020-02-11 23:02:11 +0100

    user

--- src/app/dashboard/pages/login/login.component.ts
+++ src/app/dashboard/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { UserService } from 'src/app/services/UserService/UserService';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,11 +8,27 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit, OnDestroy {
-  constructor() {}
+  email: string = '';
+  password: string = '';
+  incorrectCredentials = true;
+  constructor(private userService: UserService, private router: Router) {
+    if(localStorage.getItem('user')!=null)
+    this.router.navigateByUrl('/dashboard');
+  }
 
   ngOnInit() {
   }
   ngOnDestroy() {
   }
 
+  loginU() {
+    this.userService.checkLogin(this.email, this.password).subscribe(e => {
+      if (e.length > 0) {
+        localStorage.setItem('user', JSON.stringify(e));
+        this.router.navigateByUrl('/dashboard');
+      }
+      else
+        this.incorrectCredentials = false;
+    });
+  }
 }
--- src/app/services/UserService/UserService.ts
+++ src/app/services/UserService/UserService.ts
@@ -6,31 +6,45 @@ import { Assurance } from 'src/app/entities/Assurance';
 import { IntervenantMedicale } from 'src/app/entities/IntervenantMedicale';
 import { Parain } from 'src/app/entities/Parrain';
 import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+import { User } from 'src/app/entities/User';
+import { map, catchError } from 'rxjs/operators';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-    SuperAdminsCollection: AngularFirestoreCollection<SuperAdmin>;  
-    CompanyCollection: AngularFirestoreCollection<Company>;
-    insuranceCollection:AngularFirestoreCollection<Assurance>;
-    InterMedicalesCollection:AngularFirestoreCollection<IntervenantMedicale>;
-    ClientCollection:AngularFirestoreCollection<Parain>;
-    
-  constructor(private db:AngularFirestore) {
-    this.SuperAdminsCollection=this.db.collection('SuperAdmins');
-    
-   }
+  SuperAdminsCollection: AngularFirestoreCollection<SuperAdmin>;
+  CompanyCollection: AngularFirestoreCollection<Company>;
+  insuranceCollection: AngularFirestoreCollection<Assurance>;
+  InterMedicalesCollection: AngularFirestoreCollection<IntervenantMedicale>;
+  ClientCollection: AngularFirestoreCollection<Parain>;
 
-   addSuperAdmin()
-   {
-    
-    this.SuperAdminsCollection.add({'name':'Super Admin','email':'embeddedmajesty@gmail.com','password':'123'});
-   }
-   getUsers(){
-     let UsersList = new  Observable<any[]>();
-     
+  constructor(private db: AngularFirestore) {
+    this.SuperAdminsCollection = this.db.collection('SuperAdmins');
+    this.CompanyCollection = this.db.collection('Company');
 
-   }
+  }
+
+  addSuperAdmin() {
+
+    this.SuperAdminsCollection.add({ 'name': 'Super Admin', 'email': 'embeddedmajesty@gmail.com', 'password': '123' });
+  }
+  getUsers() {
+    let UsersList = new Observable<any[]>();
+  }
+  checkLogin(email, password) {
+    let Companies = this.db.collection('User', ref => ref.where('email', '==', email).where('password', '==', password)).snapshotChanges().pipe(
+      map(actions => actions.map(a => {
+        const data = a.payload.doc.data();
+        const id = a.payload.doc.id;
+        data['id'] = id;
+        return data;
+      }
+      ))
+    );
+    return Companies; 
+  }
+
 }
