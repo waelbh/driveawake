ff9a5f65448bffec9d40f431ac13454b3681a545
onlyifihadtime <kanzadounichway@gmail.com>
2020-02-20 00:51:26 +0100

    home

--- src/app/dashboard/pages/home/home.component.ts
+++ src/app/dashboard/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+  constructor() {
+
+  }
 
   ngOnInit() {
   }
